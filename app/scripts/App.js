21a3bd4444cd969be30168357013e8246c16a704
taraojo <tara.ojo@gmail.com>
2016-06-26 01:16:29 +0100

    Page update for deployment

--- app/scripts/App.js
+++ app/scripts/App.js
@@ -2,7 +2,44 @@ import React from 'react';
 
 class App extends React.Component {
     render() {
-        return <div>Hello</div>
+        return <footer class='footer'>
+            <div class="footer__social-links">
+                <a href="https://twitter.com/tara_ojo">
+                    <img src="#" alt="Twitter"/>
+                </a>
+                <a href="https://uk.linkedin.com/in/taraojo">
+                    <img src="#" alt="LinkedIn"/>
+                </a>
+                <a href="https://github.com/taraojo">
+                    <img src="#" alt="Github"/>
+                </a>
+                <a href="mailto:tara.ojo@gmail.com">
+                    <img src="#" alt="Email"/>
+                </a>
+            </div>
+
+            <div class="footer__site-links">
+                <a href="#">
+                    Home
+                </a>
+                <a href="#">
+                    Work
+                </a>
+                <a href="#">
+                    Thoughts
+                </a>
+                <a href="#">
+                    About
+                </a>
+                <a href="#">
+                    Contact
+                </a>
+            </div>
+
+            <div class="footer__copyright">
+                &copy; 2016 Tara Ojo
+            </div>
+        </footer>
     }
 }
 
