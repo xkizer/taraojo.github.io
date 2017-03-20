3a227df31db8ac91a855a4a93b5ba8142a687c22
taraojo <tara.ojo@gmail.com>
2017-03-20 19:28:39 +0000

    update design and content of cv

--- tasks/watch.js
+++ tasks/watch.js
@@ -4,12 +4,12 @@ var browserSync = require('browser-sync').create();
 gulp.task('serve', ['compile-sass'],function() {
     browserSync.init({
         server: {
-            baseDir: './dist/'
+            baseDir: './app/'
         }
     });
 
-    gulp.watch('./app/sass/**/*.scss',['compile-sass']);
-    gulp.watch('./app/**/*.html').on('change', ['build', browserSync.reload]);
+    gulp.watch('./app/sass/**/*.scss',['compile-sass', browserSync.reload]);
+    gulp.watch('./app/**/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['build', 'serve']);
